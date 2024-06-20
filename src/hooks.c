9bdd19a7cc3f7b39eadf45901d502339235d8199
Mostafa Omrane <momrane@paul-f4Ar4s3.clusters.42paris.fr>
2024-06-20 13:22:02 +0200

    parsing and rendering fusionnn

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 12:26:16 by momrane           #+#    #+#             */
-/*   Updated: 2024/06/20 11:25:43 by momrane          ###   ########.fr       */
+/*   Updated: 2024/06/20 13:16:56 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,44 +22,36 @@ int	ft_win_cross(t_cub3d *param)
 
 int	ft_key_hook(int keycode, t_cub3d *c)
 {
-	double	oldDirX = c->cst.dirX;
-	double	oldPlaneX = c->cst.planeX;
-
 	if (keycode == XK_Right)
 	{
+		double oldDirX = c->cst.dirX;
 		c->cst.dirX = c->cst.dirX * cos(-c->cst.rs) - c->cst.dirY * sin(-c->cst.rs);
 		c->cst.dirY = oldDirX * sin(-c->cst.rs) + c->cst.dirY * cos(-c->cst.rs);
+		double oldPlaneX = c->cst.planeX;
 		c->cst.planeX = c->cst.planeX * cos(-c->cst.rs) - c->cst.planeY * sin(-c->cst.rs);
 		c->cst.planeY = oldPlaneX * sin(-c->cst.rs) + c->cst.planeY * cos(-c->cst.rs);
 	}
 	else if (keycode == XK_Left)
-	{
-		//both camera direction and camera plane must be rotated
+{
+		double oldDirX = c->cst.dirX;
 		c->cst.dirX = c->cst.dirX * cos(c->cst.rs) - c->cst.dirY * sin(c->cst.rs);
 		c->cst.dirY = oldDirX * sin(c->cst.rs) + c->cst.dirY * cos(c->cst.rs);
+		double oldPlaneX = c->cst.planeX;
 		c->cst.planeX = c->cst.planeX * cos(c->cst.rs) - c->cst.planeY * sin(c->cst.rs);
 		c->cst.planeY = oldPlaneX * sin(c->cst.rs) + c->cst.planeY * cos(c->cst.rs);
 	}
 	else if (keycode == XK_Down)
 	{
-		printf("[%d]\n", c->data.map[(int)(c->data.px - c->cst.dirX * c->cst.ms)][(int)c->data.py]);
-		if(c->data.map[(int)(c->data.px - c->cst.dirX * c->cst.ms)][(int)c->data.py] != '0')
-		{
-			printf("go\n");
+		if(c->data.map[(int)(c->data.px - c->cst.dirX * c->cst.ms)][(int)(c->data.py)] == '0')
 			c->data.px -= c->cst.dirX * c->cst.ms;
-		}
-		printf("[%d]\n", c->data.map[(int)c->data.px][(int)(c->data.py - c->cst.dirY * c->cst.ms)]);
-		if(c->data.map[(int)c->data.px][(int)(c->data.py - c->cst.dirY * c->cst.ms)] != '0')
-		{
-			printf("go\n");
+		if(c->data.map[(int)(c->data.px)][(int)(c->data.py - c->cst.dirY * c->cst.ms)] == '0')
 			c->data.py -= c->cst.dirY * c->cst.ms;
-		}
 	}
 	else if (keycode == XK_Up)
 	{
-		if(c->data.map[(int)(c->data.px + c->cst.dirX * c->cst.ms)][(int)(c->data.py)] != '0')
+		if(c->data.map[(int)(c->data.px + c->cst.dirX * c->cst.ms)][(int)(c->data.py)] == '0')
 			c->data.px += c->cst.dirX * c->cst.ms;
-		if(c->data.map[(int)c->data.px][(int)(c->data.py + c->cst.dirY * c->cst.ms)] != '0')
+		if(c->data.map[(int)(c->data.px)][(int)(c->data.py + c->cst.dirY * c->cst.ms)] == '0')
 			c->data.py += c->cst.dirY * c->cst.ms;
 	}
 	else if (keycode == XK_Escape)
