0ecefa858e66c43eda6b0600ccabb4ad8eb4835c
vvaudain <vvaudain@student.42.fr>
2024-06-20 18:19:13 +0200

    awsd keys set

--- src/hooks.c
+++ src/hooks.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vvaudain <vvaudain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 12:26:16 by momrane           #+#    #+#             */
-/*   Updated: 2024/06/20 14:20:20 by momrane          ###   ########.fr       */
+/*   Updated: 2024/06/20 18:16:49 by vvaudain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,20 +40,54 @@ int	ft_key_hook(int keycode, t_cub3d *c)
 		c->cst.planeX = c->cst.planeX * cos(c->cst.rs) - c->cst.planeY * sin(c->cst.rs);
 		c->cst.planeY = oldPlaneX * sin(c->cst.rs) + c->cst.planeY * cos(c->cst.rs);
 	}
-	else if (keycode == XK_Down)
+	else if (keycode == XK_a || keycode == XK_A)
 	{
-		if(c->data.map[(int)(c->data.px - c->cst.dirX * c->cst.ms)][(int)(c->data.py)] == '0')
-			c->data.px -= c->cst.dirX * c->cst.ms;
-		if(c->data.map[(int)(c->data.px)][(int)(c->data.py - c->cst.dirY * c->cst.ms)] == '0')
-			c->data.py -= c->cst.dirY * c->cst.ms;
+		if (c->data.map[(int)(c->data.px - c->cst.planeX * c->cst.ms)][(int)(c->data.py)] == '0')
+			c->data.px -= c->cst.planeX * c->cst.ms;
+		if (c->data.map[(int)(c->data.px)][(int)(c->data.py - c->cst.planeY * c->cst.ms)] == '0')
+			c->data.py -= c->cst.planeY * c->cst.ms;
 	}
-	else if (keycode == XK_Up)
+	else if (keycode == XK_d || keycode == XK_D)
+	{
+		if (c->data.map[(int)(c->data.px + c->cst.planeX * c->cst.ms)][(int)(c->data.py)] == '0')
+			c->data.px += c->cst.planeX * c->cst.ms;
+		if (c->data.map[(int)(c->data.px)][(int)(c->data.py + c->cst.planeY * c->cst.ms)] == '0')
+			c->data.py += c->cst.planeY * c->cst.ms;
+	}
+	else if (keycode == XK_w || keycode == XK_W)
 	{
 		if(c->data.map[(int)(c->data.px + c->cst.dirX * c->cst.ms)][(int)(c->data.py)] == '0')
 			c->data.px += c->cst.dirX * c->cst.ms;
 		if(c->data.map[(int)(c->data.px)][(int)(c->data.py + c->cst.dirY * c->cst.ms)] == '0')
 			c->data.py += c->cst.dirY * c->cst.ms;
 	}
+	else if (keycode == XK_s || keycode == XK_S)
+	{
+		if(c->data.map[(int)(c->data.px - c->cst.dirX * c->cst.ms)][(int)(c->data.py)] == '0')
+			c->data.px -= c->cst.dirX * c->cst.ms;
+		if(c->data.map[(int)(c->data.px)][(int)(c->data.py - c->cst.dirY * c->cst.ms)] == '0')
+			c->data.py -= c->cst.dirY * c->cst.ms;
+	}
+	// else if (keycode == XK_Escape)
+	// {
+	// 	printf("Escape key pressed, so byeee !\n");
+	// 	ft_free_cube3d(c);
+	// 	exit(0);
+	// }
+	// else if (keycode == XK_Down)
+	// {
+	// 	if(c->data.map[(int)(c->data.px - c->cst.dirX * c->cst.ms)][(int)(c->data.py)] == '0')
+	// 		c->data.px -= c->cst.dirX * c->cst.ms;
+	// 	if(c->data.map[(int)(c->data.px)][(int)(c->data.py - c->cst.dirY * c->cst.ms)] == '0')
+	// 		c->data.py -= c->cst.dirY * c->cst.ms;
+	// }
+	// else if (keycode == XK_Up)
+	// {
+	// 	if(c->data.map[(int)(c->data.px + c->cst.dirX * c->cst.ms)][(int)(c->data.py)] == '0')
+	// 		c->data.px += c->cst.dirX * c->cst.ms;
+	// 	if(c->data.map[(int)(c->data.px)][(int)(c->data.py + c->cst.dirY * c->cst.ms)] == '0')
+	// 		c->data.py += c->cst.dirY * c->cst.ms;
+	// }
 	else if (keycode == XK_Escape)
 	{
 		printf("Escape key pressed, so byeee !\n");
